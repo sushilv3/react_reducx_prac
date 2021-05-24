04cd187db4343816dbbf8a75b55ed7a99436c21f
sushilv3 <sushilv3rma@gmail.com>
2021-05-24 17:47:48 +0530

    added imports HomeContainer and HeaderContainer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import HomeContainer from './containers/HomeConatiner';
+import HeaderContainer from './containers/HeaderContainer';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/**<h1>App Component</h1>*/}
+      {/*<User data={{name:"sushil" ,age :26}} />*/}
+      {/*<Home />*/}
+      <HeaderContainer />
+      <HomeContainer />
+    </>
   );
-}
+};
 
 export default App;
